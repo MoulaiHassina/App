d35058f9d8b60045aeb61390b33d1921b56a8701
MoulaiHassina <moulaihassinasafaa@outlook.com>
2021-06-08 14:02:54 +0100

    users: feed users from pre-processed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Contact;
+
+use Storage;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +19,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $json = Storage::disk('local')->get('database.json');
+        $data = json_decode($json);
+        if(is_array($data) || is_object($data))
+        {
+            foreach ($data as $obj) {
+                $contact = new Contact([
+                    'first_name' => $obj->name,
+                    'last_name' => "",
+                    'email' => $obj->email_address,
+                    'job_title' => $obj->job_title,
+                    'city' => "",
+                    'country' => "America"]);
+                $contact->save();
+        }
+    }
     }
 }
