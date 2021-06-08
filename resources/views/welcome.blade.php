77afcddcfca5dd4df43c53eed105bb9138c62d45
MoulaiHassina <moulaihassinasafaa@outlook.com>
2021-06-09 00:00:28 +0100

    secure_asset for css!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <link href="{{ sesure_asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <div class="welcome">
