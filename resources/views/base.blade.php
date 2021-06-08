63844aa3146138f6a337a4e40ced09ded8044d38
MoulaiHassina <moulaihassinasafaa@outlook.com>
2021-06-09 00:06:30 +0100

    secure_css

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,15 +3,15 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kpmg test</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <title>Laravel App</title>
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
     <div class="container">
         @yield('main') 
     </div>
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{secure_asset('js/app.js')}}"></script>
 </body>
 
 </html>
